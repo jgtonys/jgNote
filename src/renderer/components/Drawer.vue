d533d8c2bca1b6a6c4e7d470a5177f8a83c90068
jgtony <jgtony@gmail.com>
2019-06-27 01:03:30 +0900

    second commit

---
+++ src/renderer/components/Drawer.vue
@@ -0,0 +1,160 @@
+<template>
+  <v-navigation-drawer
+    id="app-drawer"
+    v-model="inputValue"
+    dark
+    app
+    fixed
+    mobile-break-point="991"
+    width="260"
+  >
+    <v-img
+      :src="image"
+      :gradient="sidebarOverlayGradiant"
+      height="100%"
+    >
+      <v-layout
+        class="fill-height"
+        tag="v-list"
+        column
+      >
+        <v-list-tile avatar>
+          <v-list-tile-avatar
+            color="white"
+          >
+
+          </v-list-tile-avatar>
+          <v-list-tile-title class="title">
+            TEST DRAWER
+          </v-list-tile-title>
+        </v-list-tile>
+        <v-divider/>
+        <v-list-tile
+          v-for="(link, i) in links"
+          :key="i"
+          :to="link.to"
+          active-class="rgba(27, 27, 27, 0.74)"
+          avatar
+          class="v-list-item"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title
+            v-text="link.text"
+          />
+        </v-list-tile>
+      </v-layout>
+    </v-img>
+  </v-navigation-drawer>
+</template>
+
+<script>
+// Utilities
+import {
+  mapMutations,
+  mapState
+} from 'vuex'
+
+export default {
+  name: 'Drawer',
+  data: () => ({
+    logo: '~@/assets/vuetifylogo.png',
+    inputValue: true,
+    image: 'https://demos.creative-tim.com/vue-material-dashboard/img/sidebar-2.32103624.jpg',
+    links: [
+      {
+        to: '/dashboard',
+        icon: 'mdi-view-dashboard',
+        text: 'Dashboard'
+      },
+      {
+        to: '/test',
+        icon: 'mdi-account',
+        text: 'User Profile'
+      },
+      {
+        to: '/table-list',
+        icon: 'mdi-clipboard-outline',
+        text: 'Table List'
+      },
+      {
+        to: '/typography',
+        icon: 'mdi-format-font',
+        text: 'Typography'
+      },
+      {
+        to: '/icons',
+        icon: 'mdi-chart-bubble',
+        text: 'Icons'
+      },
+      {
+        to: '/maps',
+        icon: 'mdi-map-marker',
+        text: 'Maps'
+      },
+      {
+        to: '/notifications',
+        icon: 'mdi-bell',
+        text: 'Notifications'
+      }
+    ],
+    responsive: true
+  }),
+  computed: {
+    ...mapState('drawer', 'color', 'sidebarBackgroundColor'),
+    /*inputValue: {
+      get () {
+        console.log("asdf");
+        return this.$store.getters.getDrawerValue
+      },
+      set (val) {
+        //this.setDrawer(val)
+      }
+    },*/
+    items () {
+      return this.$t('Layout.View.items')
+    },
+    sidebarOverlayGradiant () {
+      return `${this.$store.state.sidebarBackgroundColor}, ${this.$store.state.sidebarBackgroundColor}`
+    }
+  },
+  mounted () {
+    //this.onResponsiveInverted()
+    //window.addEventListener('resize', this.onResponsiveInverted)
+  },
+  beforeDestroy () {
+    //window.removeEventListener('resize', this.onResponsiveInverted)
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+  #app-drawer {
+    .v-list__tile {
+      border-radius: 4px;
+
+      &--buy {
+        margin-top: auto;
+        margin-bottom: 17px;
+      }
+    }
+
+    .v-image__image--contain {
+      top: 9px;
+      height: 60%;
+    }
+
+    .search-input {
+      margin-bottom: 30px !important;
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+
+    div.v-responsive.v-image > div.v-responsive__content {
+      overflow-y: auto;
+    }
+  }
+</style>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,128 +1,326 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
+	<div id="app">
 
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
-  </div>
+		<swipe-list
+			ref="list"
+			class="card"
+			:disabled="!enabled"
+			:items="mockSwipeList"
+      :key="ids"
+			:revealed.sync="revealed"
+			:item-disabled="({ disabled }) => disabled"
+			@closed="setLastEvent('closed', $event)"
+			@leftRevealed="setLastEvent('leftRevealed', $event)"
+			@rightRevealed="setLastEvent('rightRevealed', $event)"
+		>
+			<template v-slot="{ item, index, revealLeft, revealRight, close, revealed }">
+				<!-- item is the corresponding object from the array -->
+				<!-- index is clearly the index -->
+				<!-- revealLeft is method which toggles the left side -->
+				<!-- revealRight is method which toggles the right side -->
+				<!-- close is method which closes an opened side -->
+        <transition-group name="list-complete" tag="p">
+				<div ref="content" class="card-content noselect list-complete-item" @click.native="itemClick(item)" :key="item.id">
+					<!-- style content how ever you like -->
+					<h2>{{ item.title }}</h2>
+					<p><b>id:</b> {{ item.id }} <b>description:</b> {{ item.description }} <b>revealed:</b> {{ revealed || 'flase' }}</p>
+					<b>index:</b><span> {{ index }}</span>
+					<input
+						:id="`${index}disabled`"
+						v-model="item.disabled"
+						type="checkbox"
+					>
+					<label :for="`${index}disabled`">Disabled</label>
+				</div>
+        </transition-group>
+			</template>
+			<!-- left swipe side template and v-slot:left="{ item }" is the item clearly -->
+			<!-- remove if you dont wanna have left swipe side  -->
+			<template v-slot:left="{ item, close, index }">
+				<div class="swipeout-action red" title="remove" @click="remove(item)">
+					<!-- place icon here or what ever you want -->
+					<i class="fa fa-trash"></i>
+				</div>
+			</template>
+			<!-- right swipe side template and v-slot:right"{ item }" is the item clearly -->
+			<!-- remove if you dont wanna have right swipe side  -->
+			<template v-slot:right="{ item }">
+				<div class="swipeout-action blue">
+					<!-- place icon here or what ever you want -->
+					<i class="fa fa-heart"></i>
+				</div>
+			</template>
+			<template v-slot:empty>
+				<div>
+					<!-- change mockSwipeList to an empty array to see this slot in action  -->
+					list is empty ( filtered or just empty )
+				</div>
+			</template>
+		</swipe-list>
+
+	</div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
-
-  export default {
-    name: 'landing-page',
-    components: { SystemInformation },
-    methods: {
-      open (link) {
-        this.$electron.shell.openExternal(link)
-      }
-    }
-  }
+	import { SwipeList, SwipeOut } from 'vue-swipe-actions';
+	// eslint-disable-next-line no-unused-vars
+	export default {
+		name: 'App',
+		components: {
+			SwipeOut,
+			SwipeList,
+		},
+		data() {
+			return {
+				enabled: true,
+				page: 0,
+				revealed: {},
+				lastEventDescription: '',
+        ids: 0,
+				mockSwipeList: [
+						{
+							id: 'a',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						},
+						{
+							id: 'b',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						},
+						{
+							id: 'c',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						},
+						{
+							id: 'd',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						},
+						{
+							id: 'e',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						},
+						{
+							id: 'f',
+							title: 'Some title',
+							description: 'some description',
+							disabled: false,
+						}
+				],
+			};
+		},
+		mounted() {
+			// ideally should be in some global handler/store
+			window.addEventListener('keydown', this.onKeyDown);
+			window.addEventListener('keyup', this.onKeyUp);
+		},
+		beforeDestroy() {
+			window.removeEventListener('keydown', this.onKeyDown);
+			window.removeEventListener('keyup', this.onKeyUp);
+		},
+		methods: {
+			dd(v) {
+				console.log(v.target);
+			},
+			revealFirstRight() {
+				this.$refs.list.revealRight(0);
+			},
+			revealFirstLeft() {
+				this.$refs.list.revealLeft(0);
+			},
+			closeFirst() {
+				this.$refs.list.closeActions(0);
+			},
+			closeAll() {
+				this.$refs.list.closeActions();
+			},
+			remove(item) {
+				this.mockSwipeList = this.mockSwipeList.filter(i => i !== item);
+			},
+			setLastEvent(name, { item, index }) {
+				this.lastEventDescription = {
+					name,
+					index,
+					id: item.id,
+				};
+			},
+			itemClick(e) {
+				console.log(e, 'item click');
+			},
+			fbClick(e) {
+				console.log(e, 'First Button Click');
+			},
+			sbClick(e) {
+				console.log(e, 'Second Button Click');
+			},
+			// keyboard
+			onKeyDown(e) {
+				if (e.keyCode !== 16) return;
+				this.enabled = false;
+			},
+			onKeyUp(e) {
+				if (e.keyCode !== 16) return;
+				this.enabled = true;
+			},
+		},
+	};
 </script>
-
 <style>
-  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+.list-complete-item {
+  transition: all 0.3s;
+  display: inline-block;
+  margin-right: 10px;
+}
+.list-complete-enter, .list-complete-leave-to
+/* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome and Opera */
+}
+/* app specific styles */
+.swipeout-action {
+  display: flex;
+  align-items: center;
+  padding: 0 3rem;
+  cursor: pointer;
+  left: 0;
+}
+/* https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/color/ */
+.swipeout-action.blue {
+    color: white;
+    background-color: rgb(0, 122, 255);
+}
+.swipeout-action.blue:hover {
+    background-color: darken(rgb(0, 122, 255), 5%);
+}
+.swipeout-action.purple {
+    color: white;
+    background-color: rgb(88, 86, 214);
+}
+.swipeout-action.purple:hover {
+    background-color: darken(rgb(88, 86, 214), 5%);
+}
+.swipeout-action.red {
+    color: white;
+    background-color: rgb(255, 59, 48);
+}
+.swipeout-action.red:hover {
+    background-color: darken(rgb(255, 59, 48), 5%);
+}
+.swipeout-action.green {
+    color: white;
+    background-color: rgb(76, 217, 100);
+}
+.swipeout-action.green:hover {
+	background-color: darken(rgb(76, 217, 100), 5%);
+}
+.swipeout-list-item {
+  flex: 1;
+  border-bottom: 1px solid lightgray;
+}
+.swipeout-list-item:last-of-type {
+    border-bottom: none;
+}
+.card {
+  width: 100%;
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: none;
+  border: 1px solid lightgray;
+}
+.card-content {
+  padding: 1rem;
+}
+.transition-right {
+	transform: translate3d(100%, 0 ,0) !important;
+}
+.transition-left {
+	transform: translate3d(-100%, 0 ,0) !important;
+}
+.toolbar {
+	display: flex;
+	align-items: center;
+}
+.toolbar .toolbar-section {
+	flex: 0 0 auto;
+}
+.toolbar .toolbar-section--center {
+	flex: 1000 1 0%;
+}
 
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+.swipeout {
+	position: relative;
+	overflow: hidden;
+	display: flex;
+}
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+.swipeout .swipeout-left, .swipeout .swipeout-right {
+	position: absolute;
+	height: 100%;
+	display: flex;
+	z-index: 1;
+}
 
-  #wrapper {
-    background:
-      radial-gradient(
-        ellipse at top left,
-        rgba(255, 255, 255, 1) 40%,
-        rgba(229, 229, 229, .9) 100%
-      );
-    height: 100vh;
-    padding: 60px 80px;
-    width: 100vw;
-  }
+.swipeout .swipeout-left {
+	left: 0;
+	transform: translateX(-100%);
+}
 
-  #logo {
-    height: auto;
-    margin-bottom: 20px;
-    width: 420px;
-  }
+.swipeout .swipeout-right {
+	right: 0;
+	transform: translateX(100%);
+}
 
-  main {
-    display: flex;
-    justify-content: space-between;
-  }
+.swipeout .swipeout-content,
+.swipeout .swipeout-action {
+	transition: transform .2s;
+	will-change: transform;
+}
 
-  main > div { flex-basis: 50%; }
+.swipeout.swipeout--no-transition .swipeout-content,
+.swipeout.swipeout--no-transition .swipeout-action {
+  transition: none !important;
+}
 
-  .left-side {
-    display: flex;
-    flex-direction: column;
-  }
+.swipeout .swipeout-content {
+	width: 100%;
+}
 
-  .welcome {
-    color: #555;
-    font-size: 23px;
-    margin-bottom: 10px;
-  }
+.swipeout-non-selectable {
+	user-select: none !important;
+}
 
-  .title {
-    color: #2c3e50;
-    font-size: 20px;
-    font-weight: bold;
-    margin-bottom: 6px;
-  }
+.swipeout-no-pointer-events {
+	pointer-events: none !important;
+}
 
-  .title.alt {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
+/* SwipeList */
 
-  .doc p {
-    color: black;
-    margin-bottom: 10px;
-  }
+.swipeout-list {
+	display: flex;
+	flex-direction: column;
+}
 
-  .doc button {
-    font-size: .8em;
-    cursor: pointer;
-    outline: none;
-    padding: 0.75em 2em;
-    border-radius: 2em;
-    display: inline-block;
-    color: #fff;
-    background-color: #4fc08d;
-    transition: all 0.15s ease;
-    box-sizing: border-box;
-    border: 1px solid #4fc08d;
-  }
-
-  .doc button.alt {
-    color: #42b983;
-    background-color: transparent;
-  }
+.swipeout-list-item {
+	flex: 1;
+}
 </style>
